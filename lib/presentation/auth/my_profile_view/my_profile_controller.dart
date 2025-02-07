439212226322bccc823138cd49404b777b1ed9ce
Pramod Vishwakarma <pramod00108@gmail.com>
2025-02-07 16:43:02 +0530

    .

--- lib/presentation/auth/my_profile_view/my_profile_controller.dart
+++ lib/presentation/auth/my_profile_view/my_profile_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frenly_app/core/utils/pref_utils.dart';
 import 'package:frenly_app/data/repositories/api_repository.dart';
 import 'package:frenly_app/presentation/user_profile_screen/user_profile_model.dart';
 import 'package:get/get.dart';
@@ -35,68 +36,69 @@ class MyProfileController extends GetxController {
 
 void checkUserBlockMyAdminOrNot() async {
   try {
-    print("line 38");
-    if(Get.isDialogOpen==true){
-      Get.back();
-    }
-    final response = await ApiRepository.myProfile(checkUserBlock: false);
-    if (response.user?.status == 0) {
-      if(Get.isDialogOpen==true){
-        Get.back();
-      }
-      Get.dialog(
-        WillPopScope(
-          onWillPop: () async {
-            return false;
-          },
-          child: AlertDialog(
-            backgroundColor: Color(0xff001649),
-            // Match primary color
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            title: Text(
-              "access_denied".tr,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    bool isLoggedIn = PrefUtils().isLoggedIn();
+    if(isLoggedIn==true){
+      final response = await ApiRepository.myProfile(checkUserBlock: false);
+      if (response.user?.status == 0) {
+        if(Get.isDialogOpen==true){
+          Get.back();
+        }
+        Get.dialog(
+          WillPopScope(
+            onWillPop: () async {
+              return false;
+            },
+            child: AlertDialog(
+              backgroundColor: Color(0xff001649),
+              // Match primary color
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
-            ),
-            content: Text(
-              "you_have_been_blocked_by_the_admin".tr,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-              ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Get.back();
-                  onTapLogOutBtn();
-                  // logoutUser();
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  "ok".tr,
-                  style: TextStyle(
-                    color: Color(0xff001649),
-                    fontWeight: FontWeight.bold,
-                  ),
+              title: Text(
+                "access_denied".tr,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
+              content: Text(
+                "you_have_been_blocked_by_the_admin".tr,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                    onTapLogOutBtn();
+                    // logoutUser();
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    "ok".tr,
+                    style: TextStyle(
+                      color: Color(0xff001649),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        barrierDismissible: false, // Prevent dismissing by tapping outside
-      );
+          barrierDismissible: false, // Prevent dismissing by tapping outside
+        );
+      }
+
     }
+
 
   } catch (e, s) {
     print(e);
